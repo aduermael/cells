0e5529af2c98d40443fcff9aefbe837d455c255d
aduermael <adrien@duermael.com>
2025-12-17 17:11:10 -0800

    1a: Create apps/cli/BUILD with Bazel target

---
+++ apps/cli/main.cc
@@ -0,0 +1,7 @@
+// Cells CLI - spreadsheet format converter
+// Usage: cells -i <input> <output>
+
+int main(int argc, char* argv[]) {
+    // TODO: Implement argument parsing in 1b
+    return 0;
+}
